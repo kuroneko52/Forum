c28a8dbdf6fbedc8e02b7023c659c94d8f1fab5c
kuroneko52 <cruel.wonderland@outlook.com>
2025-08-17 02:14:41 +0900

    edit controller and model

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class ReplyController extends Controller
 {
     //
+    public function store(Request $request, Thread $thread)
+    {
+        $request->validate([
+            'content' => 'required',
+        ]);
+    
+        $thread->replies()->create($request->all());
+        return redirect()->route('threads.show', $thread);
+    }
+
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    protected $fillable = ['content', 'thread_id'];
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
