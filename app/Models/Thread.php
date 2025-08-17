26f16e4110d6c63ddf264557f03af6c2357e9457
kuroneko52 <cruel.wonderland@outlook.com>
2025-08-18 02:10:30 +0900

    use factory, add HasFactory

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Thread extends Model
 {
-    //
+    use HasFactory;
+    
     protected $fillable = ['title', 'content', 'name'];
 
     public function replies()
