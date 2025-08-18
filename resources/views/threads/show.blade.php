89ef48477dfc8e407bd83f1d6939973e923d530b
kuroneko52 <cruel.wonderland@outlook.com>
2025-08-18 22:46:01 +0900

    レスのリンクでレスまで飛べるようにした

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,19 +3,21 @@
 @section('content')
     <h1>{{ $thread->title }}</h1>
 
-    <p>1 : {{ $thread->created_at->format('Y/m/d H:i:s') }}</p> <!-- スレッド内容に番号1を追加 -->
-    <div class="reply">
+    <p>1 : {{ $thread->created_at->format('Y/m/d H:i:s') }}</p>
+    
+    <div class="reply" id="thread">
         <p>{{ $thread->content }}</p>
     </div>
     <hr>
-    
+
     @foreach ($thread->replies as $index => $reply)
-        <p>{{ $index + 2 }} : {{ $reply->created_at->format('Y/m/d H:i:s') }}</p> <!-- レスに番号を追加 -->
-        <div class="reply">
-            <p> {{ $reply->content }}</p>
+        <p>{{ $index + 2 }} : {{ $reply->created_at->format('Y/m/d H:i:s') }}</p>
+        <div class="reply" id="reply-{{ $reply->id }}">
+            <p>{!! convertReplyContent($reply->content, $thread) !!}</p>
         </div>
         <hr>
     @endforeach
+
 
     <h4>書き込み</h4>
     <form action="{{ route('replies.store', $thread) }}" method="POST">
@@ -31,3 +33,20 @@
         <button type="submit" class="btn btn-success">書き込む</button>
     </form>
 @endsection
+
+@php
+function convertReplyContent($content, $thread) {
+    return preg_replace_callback('/>>(\d+)/', function($matches) use ($thread) {
+        $replyNumber = (int)$matches[1];
+
+        // スレッドの内容にリンクする場合
+        if ($replyNumber === 1) {
+            return '<a href="#thread-info">>>1</a>'; // スレッドの内容へのリンク
+        }
+
+        // リンクを生成（存在しないIDでもリンクを作成）
+        return '<a href="#reply-info-' . htmlspecialchars($replyNumber) . '">>>' . htmlspecialchars($replyNumber) . '</a>';
+    }, $content);
+}
+
+@endphp
